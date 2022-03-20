3f4968c7c0fb4db2778d0fdcb81981645c00e8cb
Karen C. Aragon <saintkaren@gmail.com>
2022-03-20 17:07:51 -0400

    fixing posts

--- src/api/story-content/controllers/story-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  story-content controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::story-content.story-content');
--- src/api/story-content/routes/story-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * story-content router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::story-content.story-content');
